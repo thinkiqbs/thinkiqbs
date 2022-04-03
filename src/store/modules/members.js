a94552dfd1b205d03d51130ac8363eef9d3aec08
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-04-03 20:53:08 +0300

    --- Auto-Git Commit ---

--- src/store/modules/members.js
+++ src/store/modules/members.js
@@ -1,33 +1,34 @@
 import axios from "axios";
 
+
 const state = {
-  members: [],
-  deposits: [],
+    members: [],
+    deposits: [],
 };
 const getters = {
-  allMembers: (state) => state.members,
+    allMembers: (state) => state.members,
 
-  allDeposits: (state) => state.deposits,
+    allDeposits: (state) => state.deposits,
 };
 const actions = {
-  async fetchMembers({ commit }) {
-    const response = await axios.get("/members/api/v1/MemberDetails/");
+    async fetchMembers({ commit }) {
+        const response = await axios.get("/members/api/v1/MemberDetails/");
 
-    commit("setMembers", response.data.results);
-  },
-  async fetchDeposits({ commit }) {
-    const response = await axios.get("/members/api/v1/MonthDeposits/");
-    commit("setDeposits", response.data.results);
-  },
+        commit("setMembers", response.data.results);
+    },
+    async fetchDeposits({ commit }) {
+        const response = await axios.get("/members/api/v1/MonthDeposits/");
+        commit("setDeposits", response.data.results);
+    },
 };
 const mutations = {
-  setMembers: (state, members) => (state.members = members),
-  setDeposits: (state, deposits) => (state.deposits = deposits),
+    setMembers: (state, members) => (state.members = members),
+    setDeposits: (state, deposits) => (state.deposits = deposits),
 };
 
 export default {
-  state,
-  getters,
-  actions,
-  mutations,
+    state,
+    getters,
+    actions,
+    mutations,
 };

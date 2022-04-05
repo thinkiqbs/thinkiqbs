0a8af48db3b9a03ab8419fc4f4c5ae4039cfc474
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-04-05 19:33:07 +0300

    --- Auto-Git Commit ---

--- src/Backend/Finance/shares.vue
+++ src/Backend/Finance/shares.vue
@@ -178,10 +178,9 @@
                                 <th>Phone Number</th>
                                 <th>email</th>
                                 <th>Account Number</th>
-                                <th>Approved</th>
+                                
                                 <th>Invite</th>
-                                <th>Action</th>
-                                <th>View</th>
+                                
                               </tr>
                             </thead>
                             <tbody>
@@ -197,115 +196,25 @@
                                 <td>{{ member.email }}</td>
                                 <td>{{ member.account_no }}</td>
 
-                                <td>
-                                  <span v-if="member.Application_Status">
-                                    <i class="fas fa-check-circle"></i
-                                  ></span>
-                                  <span v-if="!member.Application_Status"
-                                    ><i
-                                      class="fa fa-warning text-warning"
-                                      style="font-size: 24px"
-                                    ></i
-                                  ></span>
-                                </td>
+                                
 
                                 <td>
-                                  <h1 v-if="member.invited == false">
+                                  <h1>
                                     <input
                                       type="checkbox"
                                       id="checkbox"
                                       v-model="member.invited"
-                                      @click="invite(member)"
+                                      @click="changemembershares(item)"
                                     />
                                   </h1>
-                                  <h5 v-else>
+                                  <h5 class="badge badge-pill badge-info">
+                                    {{ member.invited }}
                                     <label for="checkbox"></label> 👍
                                   </h5>
                                 </td>
 
-                                <td>
-                                  <div class="row">
-                                    <div
-                                      class="col-4"
-                                      v-if="member.Application_Status"
-                                    >
-                                      <a
-                                        href="#"
-                                        class="btn btn-outline-secondary"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#Contribution"
-                                        @click="newcontribution(member)"
-                                      >
-                                        <i
-                                          class="fas fa-piggy-bank"
-                                          style="color: green"
-                                        ></i>
-                                      </a>
-                                    </div>
-                                    <div
-                                      class="col-4"
-                                      v-if="member.Application_Status"
-                                    >
-                                      <button
-                                        @click="change(member)"
-                                        type="button"
-                                        class="btn btn-outline-secondary"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#loan"
-                                      >
-                                        <svg
-                                          xmlns="http://www.w3.org/2000/svg"
-                                          width="16"
-                                          height="16"
-                                          fill="currentColor"
-                                          class="bi bi-book"
-                                          viewBox="0 0 16 16"
-                                        >
-                                          <path
-                                            d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
-                                          />
-                                        </svg>
-                                      </button>
-                                    </div>
-                                    <div v-else>
-                                      <button
-                                        @click="change(product)"
-                                        type="button"
-                                        class="btn btn-outline-secondary"
-                                        data-bs-toggle="modal"
-                                        data-bs-target="#"
-                                        disabled
-                                      >
-                                        Inactive
-                                      </button>
-                                    </div>
-                                  </div>
-                                </td>
-                                <td>
-                                  <button
-                                    type="button"
-                                    class="btn btn-outline-secondary"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#MemberInfo"
-                                    @click="change(member)"
-                                  >
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      width="16"
-                                      height="16"
-                                      fill="currentColor"
-                                      class="bi bi-eye-fill"
-                                      viewBox="0 0 16 16"
-                                    >
-                                      <path
-                                        d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"
-                                      />
-                                      <path
-                                        d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
-                                      />
-                                    </svg>
-                                  </button>
-                                </td>
+                                
+                                
                               </tr>
                             </tbody>
                           </table>
@@ -512,11 +421,11 @@ export default {
       roles: [],
       path: "",
       loanschedule: [],
-      selectedmember: [],
+      selectedmember: {},
       shares: [
         {
-          DocumentID: "sh" + "mem" + this.selectedmember.id,
-          SourcedocID: "mem" + this.selectemember.id,
+          DocumentID: "sh" + "mem",
+          SourcedocID: "mem",
           accountype_description: "",
           maincode: "",
           maincode_description: "",
@@ -571,97 +480,13 @@ export default {
         console.log(error);
       });
 
-    axios
-      .get("/loans/api/v1/loans/", {
-        params: { company_id: this.companyid3 },
-      })
-      .then((res) => {
-        this.all_loans = res.data.results;
-        // console.log("myloans:",this.all_loans)
-        // $("#walla").DataTable();
-      }),
-      axios
-        .get("/loans/api/v1/loans/", {
-          params: { organizationprofile: this.orgprofileid, Status: 4 },
-        })
-        .then((res) => {
-          this.monthlyloan = res.data.results;
-          $("#walla").DataTable();
-        }),
-      axios.get("/sys_config/api/v1/EmployerProfile/").then((res) => {
-        this.employer = res.data.results;
-        // $("#example").DataTable();
-      }),
-      axios.get("/sys_config/api/v1/EmployerProfile/").then((res) => {
-        this.employer = res.data.results;
-        // $("#example").DataTable();
-      }),
-      axios
-        .get("/members/api/v1/MonthDeposits/", {
-          params: { organizationprofile: this.orgprofileid },
-        })
-        .then((res) => {
-          this.monthdeposits = res.data.results;
-          $("#monthdeposits").DataTable();
-        })
-        .catch((error) => {
-          console.error(error);
-        }),
-      axios
-        .get("/finance/api/v1/documents/", {
-          params: { Document: "deposits" },
-        })
-        .then((res) => {
-          this.totaldeposits = res.data.results;
-          $("#monthdeposits").DataTable();
-        })
-        .catch((error) => {
-          console.error(error);
-        }),
-      axios
-        .get("/finance/api/v1/documents/", {
-          params: {
-            organizationprofile: this.orgprofileid,
-            Document: "loans",
-          },
-        })
-        .then((res) => {
-          this.loanschedule = res.data.results;
-          $("#walla").DataTable();
-        }),
-      axios.get("/sys_config/api/v1/EmployerProfile/").then((res) => {
-        this.employer = res.data.results;
-        // $("#example").DataTable();
-      });
-
-    axios
-      .get("/members/api/v1/MemberDetails/", {
-        params: { organizationprofile: this.orgprofileid },
-      })
-      .then((res) => {
-        this.memberscount = res.data.count;
-        // $("#members").DataTable();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    axios
-      .get("/members/api/v1/MemberDetails/", {
-        params: {},
-      })
-      .then((res) => {
-        this.memberemail = res.data.results;
-        //   console.log("loans", this.loans)
-        $("#walla").DataTable();
-      }),
-      axios
-        .get("/loans/api/v1/loans/", {
-          params: {},
-        })
-        .then((res) => {
-          this.optionloansemail = res.data.results;
-          $("#walla").DataTable();
-        });
+    
+      
+      
+      
+      
+      
+     
   },
 
   created() {
@@ -766,21 +591,20 @@ export default {
           });
     },
 
-    changemembershares(item) {
-      this.selectedmember = item;
+    changemembershares(member) {
+      this.selectedmember = member;
+      console.log("member",this.selectedmember)
     },
 
-    changeSharesGl(){
-
-    },
+    changeSharesGl() {},
 
     addMemberShares() {
       axios
         .post("/finance/api/v1/Shares/", {
           organizationprofile: this.orgprofileid,
 
-          DocumentID: "sh" + "mem" + this.selectemember.id,
-          SourcedocID: "mem" + this.selectemember.id,
+          DocumentID: "sh" + "mem" + this.selectedmember.id,
+          SourcedocID: "mem" + this.selectedmember.id,
           accountype_description: "",
           maincode: "",
           maincode_description: "",
@@ -1009,7 +833,6 @@ export default {
 
     change(item) {
       this.loan = item;
-
 
       const memberfilter = this.loan.email;
 

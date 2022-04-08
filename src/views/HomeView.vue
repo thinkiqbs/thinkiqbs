20dba386c061bd548222f5ffe896d6d7f49f885c
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-04-08 11:18:24 +0300

    --- Auto-Git Commit ---

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -84,8 +84,27 @@
                 data-bs-target="#LoginModal"
               >
                 <span class="d-flex align-items-center">
-                  <i class="bi-chat-text-fill me-2"></i>
-                  <span class="small">Sign in</span>
+                 
+                  <b class="bold">Sign in  </b>
+                  &nbsp;
+                  &nbsp;
+                   <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-box-arrow-in-right"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                    />
+                  </svg>
                 </span>
               </button>
             </div>
@@ -120,7 +139,7 @@
             <!-- Mashead text and app badges-->
             <div class="mb-5 mb-lg-0 text-center text-lg-start">
               <h1 class="display-1 lh-1 mb-3">
-                Build your SACCO in just a few steps.
+                Get started in just a few steps.
               </h1>
               <p class="lead fw-normal text-muted mb-5">
                 Members can join a SACCO and start saving money.

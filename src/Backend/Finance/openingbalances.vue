c19ab8052c09e3417d80fe7b3b95ce30bfc377a5
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-04-29 22:51:22 +0300

    --- Auto-Git Commit ---

--- src/Backend/Finance/openingbalances.vue
+++ src/Backend/Finance/openingbalances.vue
@@ -153,6 +153,153 @@
           </div>
         </div>
 
+        <div
+          class="modal fade"
+          id="addOpeningBalance"
+          tabindex="-1"
+          aria-labelledby="feedbackModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header bg-gradient-primary-to-secondary p-4">
+                <h5
+                  class="modal-title font-alt text-white"
+                  id="feedbackModalLabel"
+                >
+                  Send feedback
+                </h5>
+                <button
+                  class="btn-close btn-close-white"
+                  type="button"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body border-0 p-4">
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- * * SB Forms Contact Form * *-->
+                <!-- * * * * * * * * * * * * * * *-->
+                <!-- This form is pre-integrated with SB Forms.-->
+                <!-- To make this form functional, sign up at-->
+                <!-- https://startbootstrap.com/solution/contact-forms-->
+                <!-- to get an API token!-->
+                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                  <!-- Name input-->
+                  <div class="form-floating mb-3">
+                    <input
+                      class="form-control"
+                      id="name"
+                      type="text"
+                      placeholder="Enter your name..."
+                      data-sb-validations="required"
+                    />
+                    <label for="name">Full name</label>
+                    <div
+                      class="invalid-feedback"
+                      data-sb-feedback="name:required"
+                    >
+                      A name is required.
+                    </div>
+                  </div>
+                  <!-- Email address input-->
+                  <div class="form-floating mb-3">
+                    <input
+                      class="form-control"
+                      id="email"
+                      type="email"
+                      placeholder="name@example.com"
+                      data-sb-validations="required,email"
+                    />
+                    <label for="email">Email address</label>
+                    <div
+                      class="invalid-feedback"
+                      data-sb-feedback="email:required"
+                    >
+                      An email is required.
+                    </div>
+                    <div
+                      class="invalid-feedback"
+                      data-sb-feedback="email:email"
+                    >
+                      Email is not valid.
+                    </div>
+                  </div>
+                  <!-- Phone number input-->
+                  <div class="form-floating mb-3">
+                    <input
+                      class="form-control"
+                      id="phone"
+                      type="tel"
+                      placeholder="(123) 456-7890"
+                      data-sb-validations="required"
+                    />
+                    <label for="phone">Phone number</label>
+                    <div
+                      class="invalid-feedback"
+                      data-sb-feedback="phone:required"
+                    >
+                      A phone number is required.
+                    </div>
+                  </div>
+                  <!-- Message input-->
+                  <div class="form-floating mb-3">
+                    <textarea
+                      class="form-control"
+                      id="message"
+                      type="text"
+                      placeholder="Enter your message here..."
+                      style="height: 10rem"
+                      data-sb-validations="required"
+                    ></textarea>
+                    <label for="message">Message</label>
+                    <div
+                      class="invalid-feedback"
+                      data-sb-feedback="message:required"
+                    >
+                      A message is required.
+                    </div>
+                  </div>
+                  <!-- Submit success message-->
+                  <!---->
+                  <!-- This is what your users will see when the form-->
+                  <!-- has successfully submitted-->
+                  <div class="d-none" id="submitSuccessMessage">
+                    <div class="text-center mb-3">
+                      <div class="fw-bolder">Form submission successful!</div>
+                      To activate this form, sign up at
+                      <br />
+                      <a
+                        href="https://startbootstrap.com/solution/contact-forms"
+                        >https://startbootstrap.com/solution/contact-forms</a
+                      >
+                    </div>
+                  </div>
+                  <!-- Submit error message-->
+                  <!---->
+                  <!-- This is what your users will see when there is-->
+                  <!-- an error submitting the form-->
+                  <div class="d-none" id="submitErrorMessage">
+                    <div class="text-center text-danger mb-3">
+                      Error sending message!
+                    </div>
+                  </div>
+                  <!-- Submit Button-->
+                  <div class="d-grid">
+                    <button
+                      class="btn btn-primary rounded-pill btn-lg disabled"
+                      id="submitButton"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <!-- Modal for Adding Shares Deposits  -->
         <div
           class="modal fade modalbox"
@@ -533,12 +680,11 @@ export default {
           })
           .then((response) => {
             console.log(response);
-            this.$swal("Posted")
+            this.$swal("Posted");
           })
           .catch((error) => {
             console.log(error.response.data);
-            this.$swal("Error",error.response.data)
-
+            this.$swal("Error", error.response.data);
           });
       }
     },
@@ -547,56 +693,52 @@ export default {
       this.$router.push("/obimports");
     },
 
-    // Post opening balance to documents 
-    postdocument(item){
-
-      const allOB = item
+    // Post opening balance to documents
+    postdocument(item) {
+      const allOB = item;
 
       getAPI
-          .post("/finance/api/v1/documents/", {
-            DocumentID: allOB.keyvalue,
-            SourcedocID: "OB" + allOB.keyvalue,
+        .post("/finance/api/v1/documents/", {
+          DocumentID: allOB.keyvalue,
+          SourcedocID: "OB" + allOB.keyvalue,
 
-            Reporting: "Balance Sheet",
+          Reporting: "Balance Sheet",
 
-            Account_type: "1000000",
-            created: "2022-04-15T08:15:27.010435Z",
+          Account_type: "1000000",
+          created: "2022-04-15T08:15:27.010435Z",
 
-            paymentnumber: null,
+          paymentnumber: null,
 
-            Account: allOB.Account,
-            user_Id: allOB.id,
-            memberemail: allOB.memberemail,
-            Transaction_date: "2021-12-31",
-            maincode_description: allOB.Accountcode_description,
-            Account_Code: allOB.Account_Code,
-            accountype_description: allOB.accountype_description,
-            Accountcode_description: allOB.Accountcode_description,
-            maincode: allOB.maincode,
-            Debit: allOB.Debit,
-            Credit: allOB.Credit,
-            Amount: allOB.Amount,
-            Document: "deposits",
-            Transaction_type: allOB.Transaction_type,
-            Posting_Date: allOB.Posting_Date,
-            allocated: allOB.allocated,
-            company_id: allOB.company_id,
-            notes: allOB.notes,
-            updatedgl: allOB.updatedgl,
-            organizationprofile: allOB.organizationprofile,
-          })
-          .then((response) => {
-            console.log(response);
-            this.$swal("Posted")
-          })
-          .catch((error) => {
-            console.log(error.response.data);
-            this.$swal("Error",error.response.data)
-
-          });
-      },
-
-  
+          Account: allOB.Account,
+          user_Id: allOB.id,
+          memberemail: allOB.memberemail,
+          Transaction_date: "2021-12-31",
+          maincode_description: allOB.Accountcode_description,
+          Account_Code: allOB.Account_Code,
+          accountype_description: allOB.accountype_description,
+          Accountcode_description: allOB.Accountcode_description,
+          maincode: allOB.maincode,
+          Debit: allOB.Debit,
+          Credit: allOB.Credit,
+          Amount: allOB.Amount,
+          Document: "deposits",
+          Transaction_type: allOB.Transaction_type,
+          Posting_Date: allOB.Posting_Date,
+          allocated: allOB.allocated,
+          company_id: allOB.company_id,
+          notes: allOB.notes,
+          updatedgl: allOB.updatedgl,
+          organizationprofile: allOB.organizationprofile,
+        })
+        .then((response) => {
+          console.log(response);
+          this.$swal("Posted");
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+          this.$swal("Error", error.response.data);
+        });
+    },
 
     updategl(item) {
       this.fetchDocuments();

a839edb178bcaa1cd14cc0e070363e7ec13f7648
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-04-29 14:32:06 +0300

    --- Auto-Git Commit ---

--- src/Backend/Members/members.vue
+++ src/Backend/Members/members.vue
@@ -102,7 +102,7 @@
                         <th>Approved</th>
                         <th>Invite</th>
                         <th>Action</th>
-                        <th>View</th>
+
                       </tr>
                     </thead>
                     <tbody>
@@ -141,82 +141,59 @@
                           <h5 v-else><label for="checkbox"></label> 👍</h5>
                         </td>
 
+                        
                         <td>
-                          <div class="row">
-                            <div class="col-4" v-if="member.Application_Status">
-                              <a
-                                href="#"
-                                class="btn btn-outline-secondary"
-                                data-bs-toggle="modal"
-                                data-bs-target="#Contribution"
-                                @click="newcontribution(member)"
-                              >
-                                <i
-                                  class="fas fa-piggy-bank"
-                                  style="color: green"
-                                ></i>
-                              </a>
-                            </div>
-                            <div class="col-4" v-if="member.Application_Status">
-                              <button
-                                @click="change(member)"
-                                type="button"
-                                class="btn btn-outline-secondary"
-                                data-bs-toggle="modal"
-                                data-bs-target="#loan"
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  fill="currentColor"
-                                  class="bi bi-book"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path
-                                    d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
-                                  />
-                                </svg>
-                              </button>
-                            </div>
-                            <div v-else>
-                              <button
-                                @click="change(product)"
-                                type="button"
-                                class="btn btn-outline-secondary"
-                                data-bs-toggle="modal"
-                                data-bs-target="#"
-                                disabled
-                              >
-                                Inactive
-                              </button>
-                            </div>
-                          </div>
-                        </td>
-                        <td>
-                          <button
-                            type="button"
-                            class="btn btn-outline-secondary"
-                            data-bs-toggle="modal"
-                            data-bs-target="#MemberInfo"
-                            @click="change(member)"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="16"
-                              height="16"
-                              fill="currentColor"
-                              class="bi bi-eye-fill"
-                              viewBox="0 0 16 16"
+                          <div class="dropdown" v-if="member.Application_Status">
+                            <a
+                              class="btn btn-secondary dropdown-toggle"
+                              href="#"
+                              role="button"
+                              id="dropdownMenuLink"
+                              data-bs-toggle="dropdown"
+                              aria-expanded="false"
                             >
-                              <path
-                                d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"
-                              />
-                              <path
-                                d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
-                              />
-                            </svg>
-                          </button>
+                              <i class="fas fa-ellipsis-h"></i>
+                            </a>
+
+                            <ul
+                              class="dropdown-menu"
+                              aria-labelledby="dropdownMenuLink"
+                            >
+                              <li>
+                                <a
+                                  class="dropdown-item"
+                                  href="#"
+
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#Contribution"
+                                  @click="newcontribution(member)"
+                                  >Pledge</a
+                                >
+                              </li>
+                              <li>
+                                <a
+                                  v-if="member.Application_Status"
+                                  class="dropdown-item"
+                                  href="#"
+                                  @click="change(member)"
+                                  type="button"
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#loan"
+                                  >Apply Loan</a
+                                >
+                              </li>
+                              <li>
+                                <a
+                                  class="dropdown-item"
+                                  href="#"
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#MemberInfo"
+                                  @click="change(member)"
+                                  >View More</a
+                                >
+                              </li>
+                            </ul>
+                          </div>
                         </td>
                       </tr>
                     </tbody>
@@ -585,7 +562,6 @@
     </div>
 
     <!-- Modal to Add Members  -->
-   
 
     <!-- Modal for Import  -->
 
@@ -2093,7 +2069,6 @@ export default {
             response;
             this.fetchMembers(this.selected);
 
-            
             // this.$router.go(); // Refreshes page
             this.$swal({
               title: "Success",
@@ -2166,7 +2141,7 @@ export default {
           })
           .then((response) => {
             response;
-            this.$swal("Member is now Active")
+            this.$swal("Member is now Active");
             // confirm("We have received your application");
             // this.$router.push({ name: "members" });
           })

2c9e65cbc79f2029e7925a86d7cb5be855cbbbdd
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-05-02 10:53:33 +0300

    --- Auto-Git Commit ---

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -131,6 +131,379 @@
             </button>
           </div>
         </div>
+        <div
+          id="ember101"
+          class="dropdown orglist-topband float-right top-item top-item-fixed-width ember-view"
+        >
+          <div id="ember102" class="ember-view">
+            <span
+              class="cursor-pointer"
+              data-ember-action=""
+              data-ember-action-103="103"
+            >
+              <div
+                class="over-flow"
+                style="display: inline-block; max-width: 155px"
+                title="BITZ IT Consulting "
+              >
+                <!---->
+                BITZ IT Consulting &nbsp;
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 165.4 512 512"
+                class="icon icon-xsmall align-top orglist-dropdown fill-siyah-black"
+              >
+                <path
+                  d="M512 331.2c0 4.6-2 9.1-6 12.1L265 523.7c-5.3 4-12.7 4-18 0L6 343.3c-6.7-5-8-14.4-3.1-21.1 5-6.7 14.4-8 21.1-3.1l232 173.7 231.9-173.7c6.7-5 16.1-3.6 21.1 3.1 2 2.6 3 5.8 3 9z"
+                ></path>
+              </svg>
+            </span>
+            <!----><!---->
+          </div>
+        </div>
+
+        <div class="dropdown d-inline-block ms-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-alt-secondary"
+            id="page-header-notifications-dropdown"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <i class="fa fa-fw fa-bell"></i>
+            <span class="text-primary">•</span>
+          </button>
+          <div
+            class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0 border-0 fs-sm"
+            aria-labelledby="page-header-notifications-dropdown"
+          >
+            <div
+              class="p-2 bg-body-light border-bottom text-center rounded-top"
+            >
+              <h5 class="dropdown-header text-uppercase">Notifications</h5>
+            </div>
+            <ul class="nav-items mb-0">
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-check-circle text-success"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">You have a new follower</div>
+                    <span class="fw-medium text-muted">15 min ago</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-plus-circle text-primary"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">1 new sale, keep it up</div>
+                    <span class="fw-medium text-muted">22 min ago</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-times-circle text-danger"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">Update failed, restart server</div>
+                    <span class="fw-medium text-muted">26 min ago</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-plus-circle text-primary"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">2 new sales, keep it up</div>
+                    <span class="fw-medium text-muted">33 min ago</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-user-plus text-success"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">You have a new subscriber</div>
+                    <span class="fw-medium text-muted">41 min ago</span>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="text-dark d-flex py-2" href="javascript:void(0)">
+                  <div class="flex-shrink-0 me-2 ms-3">
+                    <i class="fa fa-fw fa-check-circle text-success"></i>
+                  </div>
+                  <div class="flex-grow-1 pe-2">
+                    <div class="fw-semibold">You have a new follower</div>
+                    <span class="fw-medium text-muted">42 min ago</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+            <div class="p-2 border-top text-center">
+              <a class="d-inline-block fw-medium" href="javascript:void(0)">
+                <i class="fa fa-fw fa-arrow-down me-1 opacity-50"></i> Load
+                More..
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown d-inline-block ms-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-alt-secondary d-flex align-items-center"
+            id="page-header-user-dropdown"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-gear"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"
+              />
+              <path
+                d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"
+              />
+            </svg>
+            <span class="d-none d-sm-inline-block ms-2"></span>
+            <i
+              class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"
+            ></i>
+          </button>
+          <div
+            class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
+            aria-labelledby="page-header-user-dropdown"
+            style=""
+          >
+            <div
+              class="p-3 text-center bg-body-light border-bottom rounded-top"
+            >
+              <img
+                class="img-avatar img-avatar48 img-avatar-thumb"
+                src="assets/media/avatars/avatar10.jpg"
+                alt=""
+              />
+              <p class="mt-2 mb-0 fw-medium">John Smith</p>
+              <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
+            </div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_inbox.html"
+              >
+                <span class="fs-sm fw-medium">Inbox</span>
+                <span class="badge rounded-pill bg-primary ms-2">3</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_profile.html"
+              >
+                <span class="fs-sm fw-medium">Profile</span>
+                <span class="badge rounded-pill bg-primary ms-2">1</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="javascript:void(0)"
+              >
+                <span class="fs-sm fw-medium">Settings</span>
+              </a>
+            </div>
+            <div role="separator" class="dropdown-divider m-0"></div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_lock.html"
+              >
+                <span class="fs-sm fw-medium">Lock Account</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_signin.html"
+              >
+                <span class="fs-sm fw-medium">Log Out</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="dropdown d-inline-block ms-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-alt-secondary d-flex align-items-center"
+            id="page-header-user-dropdown"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-question-circle"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+              />
+              <path
+                d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"
+              />
+            </svg>
+            <span class="d-none d-sm-inline-block ms-2"></span>
+            <i
+              class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"
+            ></i>
+          </button>
+          <div
+            class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
+            aria-labelledby="page-header-user-dropdown"
+            style=""
+          >
+            <div
+              class="p-3 text-center bg-body-light border-bottom rounded-top"
+            >
+              <img
+                class="img-avatar img-avatar48 img-avatar-thumb"
+                src="assets/media/avatars/avatar10.jpg"
+                alt=""
+              />
+              <p class="mt-2 mb-0 fw-medium">John Smith</p>
+              <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
+            </div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_inbox.html"
+              >
+                <span class="fs-sm fw-medium">Inbox</span>
+                <span class="badge rounded-pill bg-primary ms-2">3</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_profile.html"
+              >
+                <span class="fs-sm fw-medium">Profile</span>
+                <span class="badge rounded-pill bg-primary ms-2">1</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="javascript:void(0)"
+              >
+                <span class="fs-sm fw-medium">Settings</span>
+              </a>
+            </div>
+            <div role="separator" class="dropdown-divider m-0"></div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_lock.html"
+              >
+                <span class="fs-sm fw-medium">Lock Account</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_signin.html"
+              >
+                <span class="fs-sm fw-medium">Log Out</span>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="dropdown d-inline-block ms-2">
+          <button
+            type="button"
+            class="btn btn-sm btn-alt-secondary d-flex align-items-center"
+            id="page-header-user-dropdown"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <img
+              class="rounded-circle"
+              src="../assets/images/faces/7.jpg"
+              alt="Header Avatar"
+              style="width: 21px"
+            />
+            <span class="d-none d-sm-inline-block ms-2">{{ this.email }}</span>
+            <i
+              class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"
+            ></i>
+          </button>
+          <div
+            class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
+            aria-labelledby="page-header-user-dropdown"
+            style=""
+          >
+            <div
+              class="p-3 text-center bg-body-light border-bottom rounded-top"
+            >
+              <img
+                class="img-avatar img-avatar48 img-avatar-thumb"
+                src="assets/media/avatars/avatar10.jpg"
+                alt=""
+              />
+              <p class="mt-2 mb-0 fw-medium">John Smith</p>
+              <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
+            </div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_inbox.html"
+              >
+                <span class="fs-sm fw-medium">Inbox</span>
+                <span class="badge rounded-pill bg-primary ms-2">3</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="be_pages_generic_profile.html"
+              >
+                <span class="fs-sm fw-medium">Profile</span>
+                <span class="badge rounded-pill bg-primary ms-2">1</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="javascript:void(0)"
+              >
+                <span class="fs-sm fw-medium">Settings</span>
+              </a>
+            </div>
+            <div role="separator" class="dropdown-divider m-0"></div>
+            <div class="p-2">
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_lock.html"
+              >
+                <span class="fs-sm fw-medium">Lock Account</span>
+              </a>
+              <a
+                class="dropdown-item d-flex align-items-center justify-content-between"
+                href="op_auth_signin.html"
+              >
+                <span class="fs-sm fw-medium">Log Out</span>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </nav>

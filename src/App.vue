116930737e8b80718fb7d86a4285177377ee706b
Nelson Adagi <67993401+thinkiqbs@users.noreply.github.com>
2022-05-02 11:53:51 +0300

    --- Auto-Git Commit ---

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Navbar v-if="!$route.meta.hideNavbar"></Navbar>
 
-  <div class="container-fluid mt-5">
+  <div class="container-fluid mt-2">
     <error />
     <div class="">
       <router-view />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,17 @@
 <template>
+
   <nav
-    class="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-1"
+    class="navbar navbar-expand-lg navbar-light bg-light shadow-sm sticky-top"
     id="mainNav"
   >
-    <div class="container">
+    <div class="container-fluid">
       <a class="navbar-brand" href="/" v-if="token == null">
         <img
           src="../assets/images/logo/IQSACCO_Logo_Blue-PhotoRoom.png"
-          width="100"
-          height="100"
+          width="250"
+          height="250"
           alt="Sacco ERP Software"
-          class="navbar-brand-img"
+          class="logo-display img-fluid"
         />
       </a>
 
@@ -47,11 +48,6 @@
               >CONTACTS</a
             >
           </li>
-          <li class="nav-item">
-            <a class="nav-link me-lg-3" href="/Profile" v-if="token != null"
-              >My Account</a
-            >
-          </li>
         </ul>
 
         <button
@@ -81,8 +77,6 @@
             </svg>
           </span>
         </button>
-
-        
 
         <button
           class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0"
@@ -155,8 +149,8 @@
               <p class="mt-2 mb-0 fw-medium">John Smith</p>
               <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
               <a class="nav-link me-lg-3" href="/Profile" v-if="token != null"
-              >My Account</a
-            >
+                >My Account</a
+              >
             </div>
             <div class="p-2">
               <a
@@ -471,81 +465,7 @@
           </div>
         </div>
 
-        <div v-if="token != null" class="dropdown d-inline-block ms-2">
-          <button
-            type="button"
-            class="btn btn-sm btn-alt-secondary d-flex align-items-center"
-            id="page-header-user-dropdown"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            <img
-              class="rounded-circle"
-              src="../assets/images/faces/7.jpg"
-              alt="Header Avatar"
-              style="width: 21px"
-            />
-            <span class="d-none d-sm-inline-block ms-2">{{ this.email }}</span>
-            <i
-              class="fa fa-fw fa-angle-down d-none d-sm-inline-block opacity-50 ms-1 mt-1"
-            ></i>
-          </button>
-          <div
-            class="dropdown-menu dropdown-menu-md dropdown-menu-end p-0 border-0"
-            aria-labelledby="page-header-user-dropdown"
-            style=""
-          >
-            <div
-              class="p-3 text-center bg-body-light border-bottom rounded-top"
-            >
-              <img
-                class="img-avatar img-avatar48 img-avatar-thumb"
-                src="assets/media/avatars/avatar10.jpg"
-                alt=""
-              />
-              <p class="mt-2 mb-0 fw-medium">John Smith</p>
-              <p class="mb-0 text-muted fs-sm fw-medium">Web Developer</p>
-            </div>
-            <div class="p-2">
-              <a
-                class="dropdown-item d-flex align-items-center justify-content-between"
-                href="be_pages_generic_inbox.html"
-              >
-                <span class="fs-sm fw-medium">Inbox</span>
-                <span class="badge rounded-pill bg-primary ms-2">3</span>
-              </a>
-              <a
-                class="dropdown-item d-flex align-items-center justify-content-between"
-                href="be_pages_generic_profile.html"
-              >
-                <span class="fs-sm fw-medium">Profile</span>
-                <span class="badge rounded-pill bg-primary ms-2">1</span>
-              </a>
-              <a
-                class="dropdown-item d-flex align-items-center justify-content-between"
-                href="javascript:void(0)"
-              >
-                <span class="fs-sm fw-medium">Settings</span>
-              </a>
-            </div>
-            <div role="separator" class="dropdown-divider m-0"></div>
-            <div class="p-2">
-              <a
-                class="dropdown-item d-flex align-items-center justify-content-between"
-                href="op_auth_lock.html"
-              >
-                <span class="fs-sm fw-medium">Lock Account</span>
-              </a>
-              <a
-                class="dropdown-item d-flex align-items-center justify-content-between"
-                href="op_auth_signin.html"
-              >
-                <span class="fs-sm fw-medium">Log Out</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        
       </div>
     </div>
   </nav>
